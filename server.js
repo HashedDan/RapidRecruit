f641ecf71d96a63f5badd2407000b26e67150d3f
HashedDan <dmangum@hashedhealth.com>
2017-09-19 18:12:32 -0500

    switched order of routes

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express()
 
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-
 app.set("port", process.env.PORT || 3001);
 
 // app.get('/', function (req, res) {
@@ -16,6 +11,9 @@ app.get('/api', function(req, res) {
 	res.json({message: "base api url"});
 })
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 
 app.listen(app.get("port"), function () {
   console.log('Server running on http://localhost:' + app.get("port"))
