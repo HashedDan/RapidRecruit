3be0f4f7d441ec27ef4a85251070e033475e7ee5
HashedDan <dmangum@hashedhealth.com>
2017-09-19 18:41:11 -0500

    working with routes

--- server.js
+++ server.js
@@ -9,13 +9,17 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 
-router.get('/', function(req, res) {
-	res.send("base url route");
-});
 
 router.get('/test', function(req, res) {
 	res.send("api test");
 });
+
+
+router.get('/', function(req, res) {
+	res.send("base url route");
+});
+
+
 
 app.listen(app.get("port"), function () {
   console.log('Server running on http://localhost:' + app.get("port"))
