56eff764aaa729ea124b9dcd59afc64d0e43998f
HashedDan <dmangum@hashedhealth.com>
2017-09-19 23:55:35 -0500

    added all the postgres stuff, not currently functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-fetch('/api', {
-  method: 'get'
-}).then(function(response) {
-  console.log(response);
-  return response;
-}).catch(function(err) {
-  console.log(err);
-});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>This is the Rapid Recruit homepage.</h1>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express()
 
 var router = express.Router();
 
+var pg = require('pg');
+var conString = "postgres://nylvbgixchinga:56ad58ff2c4552bba4c1938d65d71056298463ce29c1a0b60725c1748235601c@ec2-174-129-224-33.compute-1.amazonaws.com:5432/d47re80ao05f5k";
+
+var db = new pg.Client(conString);
+
+db.connect(conString, function(err, client, done) {
+   client.query('SELECT * FROM basic_names', function(err, result) {
+      done();
+      if(err) return console.error(err);
+      console.log(result.rows);
+   });
+});
+
 app.set("port", process.env.PORT || 3001);
 
 if (process.env.NODE_ENV === 'production') {
