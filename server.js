8f3de89ef4fb8ce1ef35439af972794a024a42ec
HashedDan <dmangum@hashedhealth.com>
2017-10-03 14:42:48 -0500

    removed client and set up test query for db

--- server.js
+++ server.js
@@ -6,13 +6,21 @@ var router = express.Router();
 var pg = require('pg');
 var conString = "postgres://nylvbgixchinga:56ad58ff2c4552bba4c1938d65d71056298463ce29c1a0b60725c1748235601c@ec2-174-129-224-33.compute-1.amazonaws.com:5432/d47re80ao05f5k?ssl=true";
 
+var db = new pg.Client(conString);
 
+db.connect();
 
 app.set("port", process.env.PORT || 3001);
 
 
 router.get('/test', function(req, res) {
-	res.send("api test");
+	db.query('SELECT * FROM basic_names', function(err, result) {
+	  console.log('id: %d and name: %s', result.rows[0].id, result.rows[0].name);
+	  var responseString = "Database successfully queried --> id: " + result.rows[0].id + "name: " + result.rows[0].name;
+	  //since the row object is just a hash, it can be accessed also as follows
+	  // console.log('name: %s and age: %d', result.rows[0]['id'], result.rows[0]['name']);
+	  res.send(JSON.stringify(result.rows));
+	});
 });
 
 
@@ -21,9 +29,7 @@ router.get('/', function(req, res) {
 });
 
 
-// var db = new pg.Client(conString);
 
-// db.connect();
 
 // db.query("INSERT INTO basic_names(id, name) values(4, $1)", ['Test']);
 
