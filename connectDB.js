ce022653f06f8d101fc30866f9faabc8848451f0
HashedDan <dmangum@hashedhealth.com>
2017-10-22 14:31:58 -0500

    reconfiguring for demo

--- connectDB.js
+++ connectDB.js
@@ -1,3 +1,3 @@
 var pg = require('pg');
-var conString = "postgres://nylvbgixchinga:56ad58ff2c4552bba4c1938d65d71056298463ce29c1a0b60725c1748235601c@ec2-174-129-224-33.compute-1.amazonaws.com:5432/d47re80ao05f5k?ssl=true";
+var conString = "postgres://g4:guqdTp5A2VSUjedF@localhost:5432/g4";
 module.exports = new pg.Client(conString);
--- server.js
+++ server.js
@@ -1,33 +1,76 @@
-var express = require('express'),
-	organizations = require('./routes/organizations'),
-	members = require('./routes/members'),
-	events = require('./routes/events'),
-	lists = require('./routes/lists'),
-	recruits = require('./routes/recruits'),
-	signin = require('./routes/signin'),
-	votes = require('./routes/votes')
+var express = require('express');
+	// organizations = require('./routes/organizations'),
+	// members = require('./routes/members'),
+	// events = require('./routes/events'),
+	// lists = require('./routes/lists'),
+	// recruits = require('./routes/recruits'),
+	// signin = require('./routes/signin'),
+	// votes = require('./routes/votes')
 
-var app = express()
+var app = express();
 
 app.set("port", process.env.PORT || 3001);
 
-app.use('/api', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+var db = require('../connectDB');
+db.connect();
 
+// app.use('/api', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   console.log("---------");
+//   next();
+// });
+
+// app.use('/', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   console.log("---------");
+//   next();
+// });
 
 /*
 	ROUTE HANDLERS
 */
-app.get('/organizations', organizations);
-app.get('/members', members);
-app.get('/events', events);
-app.get('/lists', lists);
-app.get('/recruits', recruits);
-app.get('/signin', signin);
-app.get('/votes', votes);
+// app.get('/test', function(req, res) {
+// 	db.query('SELECT * FROM organizations', function(err, result) {
+// 	  // console.log('name: %s and school: %s', result.rows[0].org_name, result.rows[0].org_school);
+// 	  // var responseString = "Database successfully queried --> name: " + result.rows[0].org_name + "school: " + result.rows[0].org_school;
+// 	  // //since the row object is just a hash, it can be accessed also as follows
+// 	  // // console.log('name: %s and age: %d', result.rows[0]['id'], result.rows[0]['name']);
+// 	  res.send(JSON.stringify(result.rows));
+// 	});
+// });
+// app.get('/organizations', organizations);
+// app.get('/members', members);
+// app.get('/events', events);
+// app.get('/lists', lists);
+// app.get('/recruits', recruits);
+// app.get('/signin', signin);
+// app.get('/votes', votes);
+
+app.get('/organizations', function(req,res) {
+	db.query('SELECT * FROM organizations', function(err, result) {
+	  res.send(JSON.stringify(result.rows));
+	});
+});
+app.get('/members', function(req,res) {
+
+});
+app.get('/events', function(req,res) {
+
+});
+app.get('/lists', function(req,res) {
+
+});
+app.get('/recruits', function(req,res) {
+
+});
+app.get('/signin', function(req,res) {
+
+});
+app.get('/votes', function(req,res) {
+
+});
 
 
 // db.query("INSERT INTO basic_names(id, name) values(4, $1)", ['Test']);
